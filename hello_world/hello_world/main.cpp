ffc59a93c0b45cc5cd150c14be53b8d0a1aadafb
Ahmet_Dundar <dundar_ahmt@hotmail.com>
2022-01-14 02:11:46 +0300

    some changes I do not remember

--- hello_world/hello_world/main.cpp
+++ hello_world/hello_world/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    std::cout << "Hello World is activated.\n" ;
+    qInfo() << "Hello World" ;
     return a.exec();
 }

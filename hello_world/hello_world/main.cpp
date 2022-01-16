d7da1ed5841037aa3126434f4cf48793dc2242d1
Ahmet_Dundar <dundar_ahmt@hotmail.com>
2022-01-16 14:09:08 +0300

    hello_world is done and function is created

--- hello_world/hello_world/main.cpp
+++ hello_world/hello_world/main.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 #include <QDebug>
 
+using namespace std ;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::cout << "Hello World is activated.\n" ;
-    qInfo() << "Hello World" ;
+    string name{""} ;
+    int age{0} ;
+
+    qInfo() << "Enter your age: " ;
+    cin >> age ;
+    qInfo() << "Enter your name: " ;
+    cin >> name ;
+
+    qInfo() << "Your name and age is: " ;
+    cout << name << " " << age << endl ;
+    qDebug() << "debug purposees" ;
+    qCritical() << "Critical message" ;
+    qWarning() << "Warning message" ;
+    qFatal("Fatal message. After this message, program is terminated." );
+
+
+
     return a.exec();
 }
